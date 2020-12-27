83060ae3f7afb99f116a5bc6c9e2670936e31db9
adamtlee <adam.lee346@gmail.com>
2020-12-27 10:40:48 -0700

    Serializer and Content Negotiation attempt

--- CityInfo/Startup.cs
+++ CityInfo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,23 @@ namespace CityInfo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+                /*
+                .AddMvcOptions(o =>
+                {
+                    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                });
+                */
+                /* .AddJsonOptions(o =>
+                {
+                    if(o.SerializerSettings.ContractResolver != null)
+                    {
+                        var castedResolver = o.SerializerSettings.ContractResolver
+                                                as DefaultContractResolver;
+                        castedResolver.NamingStrategy = null;
+                    }
+                });
+                */
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
