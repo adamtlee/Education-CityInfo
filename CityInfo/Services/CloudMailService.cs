7f1dd6ed13a30a55cf5fc9922733a6866ee21f64
adamtlee <adam.lee346@gmail.com>
2020-12-29 01:58:14 -0700

    Appsettings configuration Email Service

--- CityInfo/Services/CloudMailService.cs
+++ CityInfo/Services/CloudMailService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,12 +9,16 @@ namespace CityInfo.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@mail.com";
-        private string _mailFrom = "noreply@mail.com";
+        private readonly IConfiguration _configuration;
+        public CloudMailService(IConfiguration configuration)
+        {
+            _configuration = configuration ??
+                throw new ArgumentNullException(nameof(configuration));
+        }
 
         public void Send(string subject, string message)
         {
-            Debug.WriteLine($"Mail From {_mailFrom} to {_mailTo}, with CloudMailService.");
+            Debug.WriteLine($"Mail From {_configuration["mailSettings:mailFromAddress"]} to {_configuration["mailSettings:mailToAddress"]}, with CloudMailService.");
             Debug.WriteLine($"Subject: {subject}");
             Debug.WriteLine($"Message: {message}");
         }
